faa4d5c519f58d6c3df82fc6a81def801eb2f536
Kofi <pastorkofi101@gmail.com>
2023-06-19 15:56:31 -0400

    Build button component.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,6 +5,7 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Button'
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -20,6 +21,9 @@ class Home extends Component {
     }
     handleTextChange = (text) => {
         console.log('change text', text)
+    }
+    handleSwapCurrency = () => {
+        console.log('press swap currency');
     }
     render() {
         return (
@@ -39,6 +43,10 @@ class Home extends Component {
                     onPress={this.handlePressQuoteCurrency}
                     editable={false}
                     value={TEMP_QUOTE_PRICE}
+                />
+                <ClearButton 
+                    text="Reverse Currencies"
+                    onPress={this.handleSwapCurrency}
                 />
             </Container>
         );
