4352f642b75aed8e64beb9cf69b918671c3f92c4
Kofi <pastorkofi101@gmail.com>
2023-06-30 09:21:46 -0400

    Build LastConverted component,

---
+++ app/components/Text/styles.js
@@ -0,0 +1,10 @@
+import EStyleSheet from 'react-native-extended-stylesheet'
+
+const styles = EStyleSheet.create({
+    smallText: {
+        color: '$white',
+        fontSize: 12,
+        textAlign: 'center'
+    }
+})
+export default styles
